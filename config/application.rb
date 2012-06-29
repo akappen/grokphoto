a5acdde0ec8e3a63889b190778b34cafb6959570
Andrew Kappen <akappen@gmail.com>
2012-06-28 21:51:03 -0500

    Configured rack-cache meta and entity stores.

--- config/application.rb
+++ config/application.rb
@@ -58,8 +58,8 @@ module Grokphoto
 
     config.middleware.insert 0, 'Rack::Cache', {
       :verbose     => true,
-      :metastore   => URI.encode("file:tmp/dragonfly/cache/meta"),
-      :entitystore => URI.encode("file:tmp/dragonfly/cache/body")
+      :metastore   => Dalli::Client.new,
+      :entitystore => URI.encode("file:tmp/cache/rack/body")
     } # unless Rails.env.production?  ## uncomment this 'unless' in Rails 3.1,
                                       ## because it already inserts Rack::Cache in production
     config.middleware.insert_after 'Rack::Cache', 'Dragonfly::Middleware', :images
